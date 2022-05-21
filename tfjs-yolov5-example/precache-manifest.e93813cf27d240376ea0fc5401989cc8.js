69e20760c78afd0b228d5fa84d367f29fec747cf
Jakub Hejhal <kubic71@gmail.com>
2022-05-21 13:18:06 +0200

    init commit2

---
+++ tfjs-yolov5-example/precache-manifest.e93813cf27d240376ea0fc5401989cc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f68be34490995748dac0",
+    "url": "/tfjs-yolov5-example/static/css/main.a69439bd.chunk.css"
+  },
+  {
+    "revision": "f68be34490995748dac0",
+    "url": "/tfjs-yolov5-example/static/js/main.f68be344.chunk.js"
+  },
+  {
+    "revision": "9a8c31182f6a99ba5e4a",
+    "url": "/tfjs-yolov5-example/static/js/1.9a8c3118.chunk.js"
+  },
+  {
+    "revision": "d686f760dfaf5cb8372f",
+    "url": "/tfjs-yolov5-example/static/js/runtime~main.d686f760.js"
+  },
+  {
+    "revision": "950cfc7835ca0d177664f692fa3d3165",
+    "url": "/tfjs-yolov5-example/index.html"
+  }
+];
